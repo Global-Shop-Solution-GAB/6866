4829baedb6a4ca7ee2c8f2485eb80f73ae459e42
Service Web <sw@gssmail.com>
2024-03-19 15:17:38 -0500

    Commit changes in C:\Websites\Git\emarlow\6866\GCG_6866_RollExported.sql

---
+++ GCG_6866_RollExported.sql
@@ -0,0 +1,10 @@
+-- Database: GLOBALPTM Metadata version: 2 Exported: Mar 1, 2023
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6866_RollExported" USING 'GCG_6866_RollExported.mkd' PAGESIZE=4096 (
+ "JSS" CHAR(15) NOT NULL,
+ "Exported" CHAR(1) DEFAULT 'Y' NOT NULL );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
