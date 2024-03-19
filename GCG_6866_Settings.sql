372f211b6d8b1f372775253be4cbed84d39a2ceb
Service Web <sw@gssmail.com>
2024-03-19 15:17:38 -0500

    Commit changes in C:\Websites\Git\emarlow\6866\GCG_6866_Settings.sql

---
+++ GCG_6866_Settings.sql
@@ -0,0 +1,11 @@
+-- Database: GLOBALPTM Metadata version: 2 Exported: Mar 1, 2023
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6866_Settings" USING 'GCG_6866_Settings.mkd' PAGESIZE=4096 (
+ "FileLoc" CHAR(200),
+ "FileDest" CHAR(200),
+ "WC" CHAR(6) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
